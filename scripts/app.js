f565ff75c72eeeeef13306bf7c2e3b6a74221bcb
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-04-09 23:47:05 -0400

    added dark background and disable other buttons while modal active

--- scripts/app.js
+++ scripts/app.js
@@ -7,21 +7,27 @@ const signUpModal = document.querySelector('.signup-form-wrapper');
 const loginCloseButton = document.querySelector('.login-x');
 const signUpCloseButton = document.querySelector('.signup-x');
 
+const formContainer = document.querySelector('.form-container');
+
 console.log(loginCloseButton);
 console.log(signUpCloseButton);
 
 signUpButtonMain.addEventListener('click', () => {
   signUpModal.classList.add('display-form');
+  formContainer.classList.add('disable');
 });
 
 loginButtonMain.addEventListener('click', () => {
   loginModal.classList.add('display-form');
+  formContainer.classList.add('disable');
 });
 
 signUpCloseButton.addEventListener('click', () => {
   signUpModal.classList.remove('display-form');
+  formContainer.classList.remove('disable');
 });
 
 loginCloseButton.addEventListener('click', () => {
   loginModal.classList.remove('display-form');
+  formContainer.classList.remove('disable');
 });
